e3ef0344390512f96f585a8b29dd9f531df6a7fa
jonathanlevaillant <jonathan.levaillant@outlook.com>
2018-03-09 17:56:50 +0100

    added page up / down events

--- src/a11y-accordion-component.js
+++ src/a11y-accordion-component.js
@@ -7,6 +7,8 @@ const Accordions = (() => {
   const KEY_CODES = {
     enter: 13,
     space: 32,
+    pageUp: 33,
+    pageDown: 34,
     end: 35,
     home: 36,
     up: 38,
@@ -21,10 +23,12 @@ const Accordions = (() => {
       this.lastTrigger = this.triggers[this.triggers.length - 1];
 
       this.state = [];
+      this.currentFocusedIndex = null;
 
       this.isMultiSelectable = options.isMultiSelectable;
       this.isCollapsible = options.isCollapsible;
 
+      this.onFocus = this.onFocus.bind(this);
       this.onClick = this.onClick.bind(this);
       this.onKeydown = this.onKeydown.bind(this);
     }
@@ -64,27 +68,43 @@ const Accordions = (() => {
       this.updateAttributes(event);
     }
 
-    setFocus(event) {
-      event.preventDefault();
-
-      this.state.forEach((section) => {
-        if (event.which === KEY_CODES.up && event.target === section.trigger) section.prevTrigger.focus();
-        if (event.which === KEY_CODES.down && event.target === section.trigger) section.nextTrigger.focus();
+    onFocus(event) {
+      this.state.forEach((section, index) => {
+        if (event.target === section.trigger) {
+          this.currentFocusedIndex = index;
+        }
       });
+    }
 
-      if (event.which === KEY_CODES.home) this.firstTrigger.focus();
-      if (event.which === KEY_CODES.end) this.lastTrigger.focus();
+    setFocus(event) {
+      if (event.target.hasAttribute('data-controls')) {
+        event.preventDefault();
+
+        if (event.which === KEY_CODES.up || event.which === KEY_CODES.pageUp) {
+          this.state[this.currentFocusedIndex].prevTrigger.focus();
+        }
+        if (event.which === KEY_CODES.down || event.which === KEY_CODES.pageDown) {
+          this.state[this.currentFocusedIndex].nextTrigger.focus();
+        }
+        if (event.which === KEY_CODES.home) this.firstTrigger.focus();
+        if (event.which === KEY_CODES.end) this.lastTrigger.focus();
+      } else {
+        if (event.which === KEY_CODES.pageUp || event.which === KEY_CODES.pageDown) event.preventDefault();
+        if (event.which === KEY_CODES.pageUp) this.state[this.currentFocusedIndex].trigger.focus();
+        if (event.which === KEY_CODES.pageDown) this.state[this.currentFocusedIndex].nextTrigger.focus();
+      }
     }
 
     addAttributes() {
       this.accordion.setAttribute('role', 'presentation');
 
       this.state.forEach((section) => {
-        section.panel.setAttribute('role', 'region');
-        section.panel.setAttribute('aria-labelledby', section.trigger.id);
         section.trigger.setAttribute('role', 'button');
         section.trigger.setAttribute('tabindex', 0);
         section.trigger.setAttribute('aria-controls', section.trigger.dataset.controls);
+        section.panel.setAttribute('role', 'region');
+        section.panel.setAttribute('tabindex', -1);
+        section.panel.setAttribute('aria-labelledby', section.trigger.id);
       });
     }
 
@@ -92,9 +112,9 @@ const Accordions = (() => {
       if (event) event.preventDefault();
 
       this.state.forEach((section) => {
-        section.panel.setAttribute('aria-hidden', !section.isExpanded);
         section.trigger.setAttribute('aria-expanded', section.isExpanded);
         section.trigger.setAttribute('aria-disabled', section.isDisabled);
+        section.panel.setAttribute('aria-hidden', !section.isExpanded);
       });
     }
 
@@ -103,14 +123,15 @@ const Accordions = (() => {
       this.accordion.removeAttribute('role');
 
       this.state.forEach((section) => {
-        section.panel.removeAttribute('role');
-        section.panel.removeAttribute('aria-hidden');
-        section.panel.removeAttribute('aria-labelledby');
         section.trigger.removeAttribute('role');
         section.trigger.removeAttribute('tabindex');
         section.trigger.removeAttribute('aria-controls');
         section.trigger.removeAttribute('aria-expanded');
         section.trigger.removeAttribute('aria-disabled');
+        section.panel.removeAttribute('role');
+        section.panel.removeAttribute('tabindex');
+        section.panel.removeAttribute('aria-hidden');
+        section.panel.removeAttribute('aria-labelledby');
       });
     }
 
@@ -125,24 +146,30 @@ const Accordions = (() => {
       if (event.which === KEY_CODES.down) this.setFocus(event);
       if (event.which === KEY_CODES.home) this.setFocus(event);
       if (event.which === KEY_CODES.end) this.setFocus(event);
+      if (event.which === KEY_CODES.pageUp) this.setFocus(event);
+      if (event.which === KEY_CODES.pageDown) this.setFocus(event);
     }
 
-    addEventListeners(trigger) {
+    addEventListeners(trigger, panel) {
+      trigger.addEventListener('focus', this.onFocus);
       trigger.addEventListener('click', this.onClick);
       trigger.addEventListener('touchstart', this.onClick);
       trigger.addEventListener('keydown', this.onKeydown);
+      panel.addEventListener('keydown', this.onKeydown);
     }
 
-    removeEventListeners(trigger) {
+    removeEventListeners(trigger, panel) {
+      trigger.removeEventListener('focus', this.onFocus);
       trigger.removeEventListener('click', this.onClick);
       trigger.removeEventListener('touchstart', this.onClick);
       trigger.removeEventListener('keydown', this.onKeydown);
+      panel.removeEventListener('keydown', this.onKeydown);
     }
 
     destroy() {
-      this.triggers.forEach((trigger) => {
+      this.state.forEach((section) => {
         // remove event listeners
-        this.removeEventListeners(trigger);
+        this.removeEventListeners(section.trigger, section.panel);
       });
 
       // remove attributes
@@ -150,18 +177,26 @@ const Accordions = (() => {
     }
 
     render() {
+      let panel;
+      let isExpanded;
+
       this.triggers.forEach((trigger, index) => {
+        panel = document.getElementById(trigger.dataset.controls);
+        isExpanded = trigger.dataset.open === 'true';
+
+        if (isExpanded) this.currentFocusedIndex = index;
+
         this.state.push({
           trigger,
           prevTrigger: this.triggers[index - 1] || this.lastTrigger,
           nextTrigger: this.triggers[index + 1] || this.firstTrigger,
-          panel: document.getElementById(trigger.dataset.controls),
-          isExpanded: trigger.dataset.open === 'true',
-          isDisabled: !this.isCollapsible ? trigger.dataset.open === 'true' : false,
+          panel,
+          isExpanded,
+          isDisabled: !this.isCollapsible ? isExpanded : false,
         });
 
         // add event listeners
-        this.addEventListeners(trigger);
+        this.addEventListeners(trigger, panel);
       });
 
       // add attributes
